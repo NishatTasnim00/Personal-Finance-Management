a9fd80a004be853e090af607bade18eb0a175ce2
NishatTasnim00 <47378439+NishatTasnim00@users.noreply.github.com>
2025-11-17 02:10:42 +0600

    removed unused component

--- expense-tracker-frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import useAuthStore from '@/store/useAuthStore';
-
-const ProtectedRoute = ({ children }) => {
-  const { user } = useAuthStore();
-  return user ? children : <Navigate to="/login" />;
-}
-
-export default ProtectedRoute;
