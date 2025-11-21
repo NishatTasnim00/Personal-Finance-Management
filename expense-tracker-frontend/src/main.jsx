d80c0d961eadf862e099f34a4aedddf087779541
NishatTasnim00 <47378439+NishatTasnim00@users.noreply.github.com>
2025-11-21 12:48:42 +0600

    includeded transtack query

--- expense-tracker-frontend/src/main.jsx
+++ expense-tracker-frontend/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 const savedTheme = localStorage.getItem('theme') || 'light';
 document.documentElement.setAttribute('data-theme', savedTheme);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
