d991b95f7f2efb5e106168ce023f285767703322
NishatTasnim00 <47378439+NishatTasnim00@users.noreply.github.com>
2025-12-02 19:41:43 +0600

    centralized all income-related API calls

--- expense-tracker-frontend/src/hooks/income.js
+++ expense-tracker-frontend/src/hooks/income.js
@@ -1,6 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
 import api from "@/lib/api";
-import { toastError } from "@/lib/toast";
+import { toastSuccess, toastError } from "@/lib/toast";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useGetIncomes = ({
   period,
@@ -36,12 +36,54 @@ export const useGetIncomes = ({
       if (source) params.append("source", source);
 
       const url = `/incomes${params.toString() ? `?${params.toString()}` : ""}`;
-      const data  = await api.get(url);
-      console.log(data);
+      const data = await api.get(url);
       return data.result;
     },
     enabled,
     keepPreviousData: true,
     onError: () => toastError("Failed to load incomes"),
+  });
+};
+
+export const useUpdateIncome = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({ isEdit, id, formData }) => {
+      const payload = {
+        source: formData.source,
+        description: formData.description?.trim() || "",
+        amount: Number(formData.amount),
+        date: formData.date.toISOString().split("T")[0],
+      };
+
+      if (isEdit && id) {
+        return api.patch(`/incomes/${id}`, payload);
+      }
+      return api.post("/incomes", payload);
+    },
+
+    onSuccess: () => {
+      toastSuccess("Income saved successfully!");
+      queryClient.invalidateQueries({ queryKey: ["incomes"] });
+    },
+
+    onError: (err) => {
+      const msg = err?.response?.data?.message || "Failed to save income";
+      toastError(msg);
+    },
+  });
+};
+
+export const useDeleteIncome = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (id) => api.delete(`/incomes/${id}`),
+    onSuccess: () => {
+      toastSuccess("Income data Deleted successfully!");
+      queryClient.invalidateQueries({ queryKey: ["incomes"] });
+      document.getElementById("delete-confirmation-modal")?.close();
+    },
+    onError: () => toastError("Failed to delete!"),
   });
 };
